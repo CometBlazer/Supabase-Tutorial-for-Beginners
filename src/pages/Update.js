2292b8dd217c6dfd1bc684986c8a167055eb3f92
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-07-01 14:21:52 -0400

    chore: Add edit button to SmoothieCard component

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,7 +1,41 @@
+import { useParams, useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
+import supabase from "../config/supabaseClient"
+
 const Update = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [rating, setRating] = useState(1);
+
+  useEffect(() => {
+    const fetchSmoothie = async () => {
+      const { data, error } = await supabase
+        .from("smoothies")
+        .select()
+        .eq("id", id)
+        .single(); // select only one record
+
+      if (error) {
+        console.log(error);
+        navigate("/", { replace: true }); // replace: true will make sure that the user is redirected to the home page if there is an error
+      }
+
+      if (data) {
+        setTitle(data.title);
+        setMethod(data.method);
+        setRating(data.rating);
+        console.log(data);
+      }
+    };
+    fetchSmoothie();
+  }, [id, navigate]);
+
   return (
     <div className="page update">
-      <h2>Update</h2>
+      <h2>Update - {id}</h2>
     </div>
   )
 }
