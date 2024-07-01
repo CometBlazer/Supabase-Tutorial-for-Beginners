12c9e354d7af99b03d31502b2c55e421f76a5a4f
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-07-01 15:54:57 -0400

    chore: Update Home.js to include smoothie ordering functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,55 +1,55 @@
-import supabase from "../config/supabaseClient";
-import { useEffect, useState } from "react";
+import supabase from '../config/supabaseClient'
+import { useEffect, useState } from 'react'
 
-import SmoothieCard from "../components/SmoothieCard";
+// components
+import SmoothieCard from '../components/SmoothieCard'
 
 const Home = () => {
-  // console.log(supabase);
-  const [fetchError, setFetchError] = useState(null);
-  const [smoothies, setSmoothies] = useState(null);
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+  const [orderBy, setOrderBy] = useState('created_at')
 
   const handleDelete = (id) => {
     setSmoothies(prevSmoothies => {
-      return prevSmoothies.filter(sm => sm.id !== id);
+      return prevSmoothies.filter(sm => sm.id !== id)
     })
   }
 
   useEffect(() => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase
-        .from("smoothies")
+        .from('smoothies')
         .select()
-
+        .order(orderBy, {ascending: false})
+      
       if (error) {
-        setFetchError('Could not fetch the data');
-        console.log(error);
-        setSmoothies(null);
+        setFetchError('Could not fetch the smoothies')
+        setSmoothies(null)
       }
-
       if (data) {
-        setSmoothies(data);
-        setFetchError(null);
+        setSmoothies(data)
+        setFetchError(null)
       }
     }
-    fetchSmoothies();
-  }, [])
 
-  console.log(smoothies);
+    fetchSmoothies()
+
+  }, [orderBy])
 
   return (
     <div className="page home">
-      {/* <h2>Home</h2> */}
-      {fetchError && (
-        <p>{fetchError}</p>
-      )}
+      {fetchError && (<p>{fetchError}</p>)}
       {smoothies && (
         <div className="smoothies">
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy('created_at')}>Time Created</button>
+            <button onClick={() => setOrderBy('title')}>Title</button>
+            <button onClick={() => setOrderBy('rating')}>Rating</button>
+          </div>
           <div className="smoothie-grid">
-            {/* Order-by buttons */}
             {smoothies.map(smoothie => (
-              <div key={smoothie.id} className="smoothie">
-                <SmoothieCard key={smoothie.id} smoothie={smoothie} onDelete={handleDelete} />
-              </div>
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} onDelete={handleDelete} />
             ))}
           </div>
         </div>
