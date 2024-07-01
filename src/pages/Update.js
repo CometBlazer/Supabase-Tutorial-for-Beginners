16d0a7efbdd9be3af18ce9442a3a4aa31f2566ae
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-07-01 14:32:01 -0400

    chore: Update Update.js to include form for updating smoothie recipe

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,67 +1,91 @@
-import { useParams, useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
+import { useParams, useNavigate } from 'react-router-dom'
 import supabase from "../config/supabaseClient"
 
 const Update = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
+  const { id } = useParams()
+  const navigate = useNavigate()
 
-  const [title, setTitle] = useState("");
-  const [method, setMethod] = useState("");
-  const [rating, setRating] = useState(1);
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState('')
+  const [formError, setFormError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!title || !method || !rating) {
+      setFormError('Please fill in all the fields correctly.')
+      return
+    }
+
+    const { data, error } = await supabase
+      .from('smoothies')
+      .update({ title, method, rating })
+      .eq('id', id)
+      .select()
+
+    if (error) {
+      setFormError('Please fill in all the fields correctly.')
+    }
+    if (data) {
+      setFormError(null)
+      navigate('/')
+    }
+  }
 
   useEffect(() => {
     const fetchSmoothie = async () => {
       const { data, error } = await supabase
-        .from("smoothies")
+        .from('smoothies')
         .select()
-        .eq("id", id)
-        .single(); // select only one record
+        .eq('id', id)
+        .single()
 
       if (error) {
-        console.log(error);
-        navigate("/", { replace: true }); // replace: true will make sure that the user is redirected to the home page if there is an error
+        navigate('/', { replace: true })
       }
-
       if (data) {
-        setTitle(data.title);
-        setMethod(data.method);
-        setRating(data.rating);
-        console.log(data);
+        setTitle(data.title)
+        setMethod(data.method)
+        setRating(data.rating)
       }
-    };
-    fetchSmoothie();
-  }, [id, navigate]);
+    }
+
+    fetchSmoothie()
+  }, [id, navigate])
 
   return (
     <div className="page create">
-    <form>
-      <label htmlFor="title">Title:</label>
-      <input 
-        type="text" 
-        id="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input 
+          type="text" 
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-      <label htmlFor="method">Method:</label>
-      <textarea 
-        id="method"
-        value={method}
-        onChange={(e) => setMethod(e.target.value)}
-      />
+        <label htmlFor="method">Method:</label>
+        <textarea 
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
 
-      <label htmlFor="rating">Rating:</label>
-      <input 
-        type="number"
-        id="rating"
-        value={rating}
-        onChange={(e) => setRating(e.target.value)}
-      />
+        <label htmlFor="rating">Rating:</label>
+        <input 
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
 
-      <button>Update Smoothie Recipe</button>
-    </form>
-  </div>
+        <button>Update Smoothie Recipe</button>
+
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
   )
 }
 
